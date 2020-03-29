c7309cff5a6abf06cfe9405c507ad57b202239c4
jmargielewski <kuba.margielewski@gmail.com>
2020-03-29 14:58:08 +0100

    pre-commit config

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const App = (name): JSX.Element => {
+interface Props {
+  name: string;
+}
+
+const App = (props: Props): JSX.Element => {
   const jsx = (
     <div>
-      <h1>{name}</h1>
+      <h1>{props.name}</h1>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa nobis repudiandae distinctio,
       natus libero ex! Doloribus est dolorem ratione perferendis, illum, ab a odio officiis commodi
       voluptatem maiores ex vel?
