82ed86a21e8e4cced63dd73d07d2749d4ebad798
jmargielewski <kuba.margielewski@gmail.com>
2020-03-29 15:05:42 +0100

    -

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import React from 'react';
 
-interface Props {
-  name: string;
-}
-
-const App = (props: Props): JSX.Element => {
+const App = (): JSX.Element => {
   const jsx = (
     <div>
-      <h1>{props.name}</h1>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa nobis repudiandae distinctio,
       natus libero ex! Doloribus est dolorem ratione perferendis, illum, ab a odio officiis commodi
       voluptatem maiores ex vel?
