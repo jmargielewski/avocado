6d34d7b0f55ab3fcbeb176ccf76fd3a600d88958
jmargielewski <kuba.margielewski@gmail.com>
2020-03-29 13:45:46 +0100

    base configuration

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+  const jsx = (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa nobis repudiandae distinctio,
+      natus libero ex! Doloribus est dolorem ratione perferendis, illum, ab a odio officiis commodi
+      voluptatem maiores ex vel?
+    </div>
+  );
+  return jsx;
+};
+
+export default App;
