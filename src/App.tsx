0d754d63ae2de4dcf06e6f91025d32c535e02bb3
jmargielewski <kuba.margielewski@gmail.com>
2020-03-29 14:08:32 +0100

    eslint roles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
-const App = () => {
+const App = (name): JSX.Element => {
   const jsx = (
     <div>
+      <h1>{name}</h1>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa nobis repudiandae distinctio,
       natus libero ex! Doloribus est dolorem ratione perferendis, illum, ab a odio officiis commodi
       voluptatem maiores ex vel?
