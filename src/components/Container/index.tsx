fe63d6f3a2597c9c3a389a16094412bb862b67db
jmargielewski <kuba.margielewski@gmail.com>
2020-04-30 22:42:06 +0100

    update container

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -2,12 +2,15 @@ import React, { FunctionComponent } from 'react';
 import Styled from './Container.Styles';
 import { ContainerProps } from './Container.Types';
 
-const Container: FunctionComponent<ContainerProps> = ({ children, fluid }): JSX.Element => (
-  <Styled.Container fluid={fluid}>{children}</Styled.Container>
+const Container: FunctionComponent<ContainerProps> = ({ children, fluid, flex }): JSX.Element => (
+  <Styled.Container fluid={fluid} flex={flex}>
+    {children}
+  </Styled.Container>
 );
 
 Container.defaultProps = {
   fluid: false,
+  flex: false,
 };
 
 export default Container;
