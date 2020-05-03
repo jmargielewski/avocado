ff05b2b2985532e9e326549bf9fc98f5f03773f9
jmargielewski <kuba.margielewski@gmail.com>
2020-05-03 20:10:01 +0100

    provide global styles to Storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,6 +3,12 @@ import { ThemeProvider } from 'styled-components';
 import { addDecorator } from '@storybook/react';
 import StoryRouter from 'storybook-react-router';
 import { theme } from '../src/theme/mainTheme';
+import GlobalStyle from '../src/theme/GlobalStyle';
 
-addDecorator((story) => <ThemeProvider theme={theme}>{story()}</ThemeProvider>);
+addDecorator((story) => (
+  <ThemeProvider theme={theme}>
+    <GlobalStyle theme={theme} />
+    {story()}
+  </ThemeProvider>
+));
 addDecorator(StoryRouter());
