fbbbf399395976041c6b438b365e4bc281551cac
jmargielewski <kuba.margielewski@gmail.com>
2020-05-08 21:30:52 +0100

    create Dashboard route component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,34 @@
 import React, { FunctionComponent } from 'react';
-import { BrowserRouter as Router, Switch, Route, RouteProps } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, RouteProps, Redirect } from 'react-router-dom';
 
 import Root from './Root';
-import Page from './pages/Page';
+
 import MainTemplate from './templates/MainTemplate';
 import AuthTemplate from './templates/AuthTemplate';
+import DashboardTemplate from './templates/DashboardTemplate';
 
+import HomePage from './pages/HomePage';
+import DashboardPage from './pages/Dashboard';
 import LoginPage from './pages/Auth/LoginPage';
 import RegisterPage from './pages/Auth/RegisterPage';
 
-import { routes, RouteLayoutsTypes, RouteComponentsTypes } from './routes';
+import { getToken } from './localStorage/token';
+
+import {
+  routes,
+  DashboardComponentsTypes,
+  // DashboardLayoutTypes,
+  PageComponentsTypes,
+  PageLayoutsTypes,
+} from './routes';
 
 interface PageRouteProps extends RouteProps {
-  component: RouteComponentsTypes;
-  layout: RouteLayoutsTypes;
+  component: PageComponentsTypes;
+  layout: PageLayoutsTypes;
+}
+
+interface DashboardRouteProps extends RouteProps {
+  component: DashboardComponentsTypes;
 }
 
 const PageRoute: FunctionComponent<PageRouteProps> = ({
@@ -31,14 +46,33 @@ const PageRoute: FunctionComponent<PageRouteProps> = ({
   />
 );
 
+const DashboardRoute: FunctionComponent<DashboardRouteProps> = ({
+  component: Component,
+  ...rest
+}): JSX.Element => (
+  <Route
+    {...rest}
+    render={(props): JSX.Element => {
+      return getToken() ? (
+        <DashboardTemplate>
+          <Component {...props} />
+        </DashboardTemplate>
+      ) : (
+        <Redirect to={routes.login} />
+      );
+    }}
+  />
+);
+
 const App: FunctionComponent = (): JSX.Element => (
   <Root>
     <Router>
       <MainTemplate>
         <Switch>
+          <Route exact path={routes.home} component={HomePage} />
           <PageRoute exact path={routes.login} component={LoginPage} layout={AuthTemplate} />
           <PageRoute exact path={routes.register} component={RegisterPage} layout={AuthTemplate} />
-          <Route path={routes.home} component={Page} />
+          <DashboardRoute path={routes.dashboard} component={DashboardPage} />
         </Switch>
       </MainTemplate>
     </Router>
