a3e086fe09c69e7bb2bf145708dab6cf1c77f162
jmargielewski <kuba.margielewski@gmail.com>
2020-05-08 21:18:15 +0100

    update types of route pages and templates

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,22 @@
-import { RegisterPageType, LoginPageType } from '../pages/Auth';
+// Pages Types
+// import { HomePageType } from '../pages/HomePage';
+import { DashboardPageType } from '../pages/Dashboard';
+import { LoginPageType, RegisterPageType } from '../pages/Auth';
+
+// Templates Types
 import { AuthTemplateType } from '../templates/AuthTemplate';
+import { DashboardTemplateType } from '../templates/DashboardTemplate';
+
+export type DashboardComponentsTypes = DashboardPageType;
+export type DashboardLayoutTypes = DashboardTemplateType;
+
+type AuthComponentsTypes = LoginPageType | RegisterPageType;
+export type PageComponentsTypes = AuthComponentsTypes;
+export type PageLayoutsTypes = AuthTemplateType;
 
 export enum routes {
   home = '/',
   login = '/login',
   register = '/register',
+  dashboard = '/dashboard',
 }
-
-export type AuthComponentsTypes = LoginPageType | RegisterPageType;
-
-export type RouteComponentsTypes = AuthComponentsTypes;
-export type RouteLayoutsTypes = AuthTemplateType;
